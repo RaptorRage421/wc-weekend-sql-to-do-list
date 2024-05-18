610e52e60511eceeb896ebbeac05f3292313509c
Sam Shacter <sshacter@gmail.com>
2024-05-18 14:50:16 -0500

    able to add new todos correctly, with a toggle checkbox for urgent

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,7 @@
 console.log('JS is sourced!');
-console.log( 'js' );
 
 function getTodos(){
-  // console.log( 'in gettodos' );
+  console.log( 'in gettodos' );
   // axios call to server to get todos
   axios({
       method: 'GET',
@@ -10,7 +9,8 @@ function getTodos(){
   })
   .then((response) => {
     // console.log('get todos is running...', response.data)
-    renderTodo(response.data)
+    renderTodos(response.data)
+    console.log("GET /todos working" , response.data)
   })
   .catch((err) => {
     console.error('Error in /GET todos', err)
@@ -20,48 +20,24 @@ function getTodos(){
 
 
 getTodos();
-function addTodos(todoToAdd){
-  axios({
-    method: 'POST',
-    url: '/todos',
-    data: todoToAdd
-  })
-  .then((response) => {
-    // console.log('addtodo() is working...', response.data)
-    getTodos()
-  })
-  .catch((err) => {
-    console.error('Error in POST', err)
-      alert('Unable to add todo at this time. Please try again later.');
-  })
-  }
+
 
 function submitTodo(event) {
   event.preventDefault();
-//   let warningText = document.querySelector('#required_field')
-  console.log('Submit button clicked.');
-  let todo = {};
- 
-//   todo.name = document.getElementById('nameIn').value;
-//   todo.age = document.getElementById('ageIn').value;
-//   todo.favorite_color = document.getElementById('colorIn').value;
-//   todo.ready_for_transfer = document.getElementById('readyForTransferIn').value;
-//   todo.notes = document.getElementById('notesIn').value;
-//   // console.log('todo object: ', todo)
-//   if (todo.age.length === 0 || todo.name.length === 0 || todo.favorite_color.length === 0 || todo.ready_for_transfer.length === 0 || todo.notes.length === 0){
-//     return
-//   }
-//   document.getElementById('nameIn').value = '';
-//   document.getElementById('ageIn').value = '';
-//   document.getElementById('colorIn').value = '';
-//   document.getElementById('readyForTransferIn').value = '';
-//   document.getElementById('notesIn').value = '';
-  
-    addTodo(todo)
-    
 
+  console.log('Submit button clicked.');
+  let toDoHolding = {}
+  console.log("todo object," ,toDoHolding)
+ 
+  toDoHolding.text = document.getElementById('todo_text').value;
+  toDoHolding.isComplete = false
+  toDoHolding.isUrgent = document.getElementById('isUrgent').checked;
+  console.log("todo object," ,toDoHolding)
+    addTodos(toDoHolding)   
 }
-function addTodo(todoToAdd){
+
+
+function addTodos(todoToAdd){
 axios({
   method: 'POST',
   url: '/todos',
@@ -77,141 +53,23 @@ axios({
 })
 }
 
-function renderTodo(todos) {
+function renderTodos(todos) {
   const todoLocation = document.getElementById('todoListLocation')
   todoLocation.innerHTML = '';
 
   for (let i = 0; i < todos.length; i += 1) {
     let todo = todos[i];
-    // console.log('todo is ', todo)
+    console.log('todo is ', todo)
   
 
     todoLocation.innerHTML += `
       <tr>
       <td>${todo.text}</td>  
       <td>${todo.isComplete}</td>
+      <td>${todo.isUrgent}</td>
       </tr>
     `;
 
 
   }
-}
-
-// function markReady(todoId, isReady){
-//   console.log("Changing status of...", todoId, isReady);
-//   axios({
-//    method: "PUT",
-//    url: "/todos/ready/" + todoId,
-//    data: {ready_for_transfer: isReady}
-//   })
-//   .then((response) => {
-//    gettodos()
-//   })
-//   .catch((error) => {
-//    console.log('Error', error);
-//    alert('Something went wrong');
-//   });
-//   }
-
-//  function editNotes(todoId, incNotes){
-//   if (incNotes.length === 0){
-//     return
-//   }
-// axios({
-//   method: "PUT",
-//   url: "/todos/notes/" + todoId,
-//   data: {notes: incNotes}
-// })
-
-// .then((response) => {
-//   console.log('Updating todo: ',todoId+ ": "+ incNotes)
-//   gettodos()
-//   document.getElementById('notesIn').value = ''
-//  })
-//  .catch((error) => {
-//   console.log('Error', error);
-//   alert('Something went wrong');
-//  });
-//  }
-
- 
-//  function editColor(todoId, incColor){
-//   if (incColor.length === 0){
-//     return
-//   }
-// axios({
-//   method: "PUT",
-//   url: "/todos/color/" + todoId,
-//   data: {favorite_color: incColor}
-// })
-
-// .then((response) => {
-//   console.log('Updating todo: ',todoId+ ": "+ incColor)
-//   gettodos()
-//   document.getElementById('colorIn').value = ''
-//  })
-//  .catch((error) => {
-//   console.log('Error', error);
-//   alert('Something went wrong');
-//  });
-//  }
-
-//  function editAge(todoId, incAge){
-//   if (incAge.length === 0){
-//     return
-//   }
-// axios({
-//   method: "PUT",
-//   url: "/todos/age/" + todoId,
-//   data: {age: incAge}
-// })
-
-// .then((response) => {
-//   console.log('incoming age', incAge)
-//   gettodos()
-//   document.getElementById('ageIn').value = ''
-//  })
-//  .catch((error) => {
-//   console.log('Error', error);
-//   alert('Something went wrong');
-//  });
-//  }
-
-//  function editName(todoId, incName){
-//   if (incName.length === 0){
-//     return
-//   }
-// axios({
-//   method: "PUT",
-//   url: "/todos/name/" + todoId,
-//   data: {name: incName}
-// })
-
-// .then((response) => {
-//   console.log('incoming name', incName)
-//   gettodos()
-//   document.getElementById('nameIn').value = ''
-//  })
-//  .catch((error) => {
-//   console.log('Error', error);
-//   alert('Something went wrong');
-//  });
-//  }
-
-
-
-function deleteTodo(todoId) {
-
-  axios({
-    method: "DELETE",
-    url: `/todos/${todoId}`
-  })
-    .then((response) => {
-      console.log('Deleting todo: ',todoId)
-      getTodos();
-    })
-    .catch((error) => {
-      console.log('Error', error);
-      alert('Something went wrong');
-    });
 }
--- server/routes/todos.router.js
+++ server/routes/todos.router.js
@@ -18,17 +18,19 @@ todoRouter.get('/', (req, res)=>{
 // POST
 todoRouter.post('/', (req, res)=>{
     let newTodo = req.body
-    let todoArray = [newTodo.text, newTodo.isComplete]
+    console.log("new Todo", newTodo)
+    let todoArray = [newTodo.text, newTodo.isComplete, newTodo.isUrgent]
     // console.log('Adding todo', todoArray)
 
     let queryText = `
 
-    INSERT INTO "todo" ("text", "isComplete")
+    INSERT INTO "todos" ("text","isComplete", "isUrgent")
 
-    VALUES ($1, $2);
+    VALUES ($1,$2,$3);
     `
     pool.query(queryText, todoArray)
         .then((result)=>{
+            console.log(todoArray)
             res.sendStatus(201)
         })
         .catch((err)=>{
@@ -38,134 +40,44 @@ todoRouter.post('/', (req, res)=>{
 })
 
 // PUT
-// todoRouter.put('/ready/:id', (req, res)=>{
-//     let todoId = req.params.id
-//     let isReady = req.body.ready_for_transfer
-//     // console.log("req.body", req.body)
-// // console.log("is ready?" , isReady)
-// // console.log("todo id", todoId)
-//     let queryText = ''
-
-//     if (isReady === true){
-//         queryText = `
-//         UPDATE "todo" SET "ready_for_transfer"=true
-//         WHERE "id"=$1;
-//         `;
-//     } 
-//     else if(isReady === false){
-//         queryText = `
-//         UPDATE "todo" SET "ready_for_transfer"=false
-//         WHERE "id"=$1;
-//         `;
-//     }    
-//     else {
-//         res.sendStatus(500)
-//         console.error('Trouble marking as ready')
-//     }
-
-//     pool.query(queryText, [todoId])
-//         .then(()=>{
-//             res.sendStatus(204)
-//         })
-//         .catch((err)=>{
-//             console.log(`Error making query ${queryText}`, err)
-//             res.send(500)
-//         })
-// })
-
-// todoRouter.put('/name/:id', (req, res)=>{
-//     let todoId = req.params.id
-//     let incName = req.body.name
-//     console.log("req.body", req.body)
-// console.log("name." , incName)
+todoRouter.put('/complete/:id', (req, res)=>{
+    let todoId = req.params.id
+    let isComplete = req.body.isComplete
+    // console.log("req.body", req.body)
+// console.log("is ready?" , isComplete)
 // console.log("todo id", todoId)
-//     let queryText = ''
+    let queryText = ''
 
-    
-//         queryText = `
-//         UPDATE "todo" SET "name"=$2
-//         WHERE "id"=$1;
-//         `;
-    
-    
-//     pool.query(queryText, [todoId,incName])
-//         .then(()=>{
-//             res.sendStatus(204)
-//         })
-//         .catch((err)=>{
-//             console.log(`Error making query ${queryText}`, err)
-//             res.send(500)
-//         })
-// })
+    if (isComplete === true){
+        queryText = `
+        UPDATE "todo" SET "isComplete"=true
+        WHERE "id"=$1;
+        `;
+    } 
+    else if(isComplete === false){
+        queryText = `
+        UPDATE "todo" SET "isComplete"=false
+        WHERE "id"=$1;
+        `;
+    }    
+    else {
+        res.sendStatus(500)
+        console.error('Trouble marking as ready')
+    }
 
-// todoRouter.put('/age/:id', (req, res)=>{
-//     let todoId = req.params.id
-//     let incAge = req.body.age
-//     console.log("req.body", req.body)
-// console.log("age." , incAge)
-// console.log("todo id", todoId)
-//     let queryText = ''
+    pool.query(queryText, [todoId])
+        .then(()=>{
+            res.sendStatus(204)
+        })
+        .catch((err)=>{
+            console.log(`Error making query ${queryText}`, err)
+            res.send(500)
+        })
+})
 
-    
-//         queryText = `
-//         UPDATE "todo" SET "age"=$2
-//         WHERE "id"=$1;
-//         `;
-    
-    
-//     pool.query(queryText, [todoId,incAge])
-//         .then(()=>{
-//             res.sendStatus(204)
-//         })
-//         .catch((err)=>{
-//             console.log(`Error making query ${queryText}`, err)
-//             res.send(500)
-//         })
-// })
 
-// todoRouter.put('/color/:id', (req,res) => {
-//         let todoId = req.params.id
-//         let incColor = req.body.favorite_color
-//         let queryText = ''
-//         queryText = `
-//         UPDATE "todo" SET "favorite_color" = $2
-//         WHERE "id"=$1;`
-// pool.query(queryText, [todoId, incColor])
-// .then(()=>{
-//     res.sendStatus(204)
-// })
-// .catch((err)=>{
-//     console.log(`Error making query ${queryText}`, err)
-//     res.send(500)
-// })
-// })
 
-// todoRouter.put('/notes/:id', (req, res)=>{
-//     let todoId = req.params.id
-//     let incNotes = req.body.notes
-//     console.log("req.body", req.body)
-// console.log("notes." , incNotes)
-// console.log("todo id", todoId)
-//     let queryText = ''
-
-    
-//         queryText = `
-//         UPDATE "todo" SET "notes"=$2
-//         WHERE "id"=$1;
-//         `;
-    
-    
-//     pool.query(queryText, [todoId,incNotes])
-//         .then(()=>{
-//             res.sendStatus(204)
-//         })
-//         .catch((err)=>{
-//             console.log(`Error making query ${queryText}`, err)
-//             res.send(500)
-//         })
-// })
-
-// // DELETE
+// DELETE
 
 todoRouter.delete('/:id', (req,res)=>{
     console.log("req params", req.params)
