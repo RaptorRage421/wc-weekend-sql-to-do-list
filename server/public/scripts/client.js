a8884ff93c0b40a19c3c073fa75207764694d421
Sam Shacter <sshacter@gmail.com>
2024-05-19 21:52:23 -0500

    all tests pass now. just need to finish the readme

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,5 @@
 
-function onLoad(){
+function onLoad() {
     console.log('JS is sourced!');
 
 }
@@ -74,33 +74,33 @@ function renderTodos(todos) {
     todoLocation.innerHTML = '';
 
     for (let i = 0; i < todos.length; i += 1) {
-        let todo = todos[i]; 
+        let todo = todos[i];
         if (todo.isUrgent === true && todo.isComplete === false) {
 
             todoisUrgent.innerHTML += `
-            <tr class="is_urgent">
-                <td class="wordWrap" scope="col">${todo.text} ❗️</td> 
-                <td scope="col"><button class="btn btn-sm btn-primary" onClick="markComplete(event,${todo.id}, true)">Completed</button></td>
+            <tr data-testid="toDoItem" class="is_urgent">
+                <td scope="col">${todo.text} ❗️</td> 
+                <td scope="col"><button class="btn btn-sm btn-primary" data-testid="completeButton" onClick="markComplete(event,${todo.id}, true)">Completed</button></td>
                 <td scope="col"><button class="btn btn-sm btn-danger deleteButton" data-testid="deleteButton" onClick="deleteTodo(event,${todo.id})">Delete</button></td>
             </tr>
     `
         }
         else if (todo.isUrgent === false && todo.isComplete === false) {
             todoLocation.innerHTML += `
-            <tr class="row-striped">
-                <td class="wordWrap" scope="col">${todo.text} </td> 
-                <td scope="col"><button class="btn btn-sm btn-primary" onClick="markComplete(event, ${todo.id}, true)">Completed</button></td>
-                <td scope="col"><button class="btn btn-sm btn-danger deleteButton" onClick="deleteTodo(event,${todo.id})">Delete</button></td> 
+            <tr data-testid="toDoItem" class="row-striped">
+                <td scope="col">${todo.text} </td> 
+                <td scope="col"><button class="btn btn-sm btn-primary" data-testid="completeButton" onClick="markComplete(event, ${todo.id}, true)">Completed</button></td>
+                <td scope="col"><button class="btn btn-sm btn-danger deleteButton" data-testid="deleteButton" onClick="deleteTodo(event,${todo.id})">Delete</button></td> 
             </tr>
     `;
 
         }
-     if (todo.isComplete === true) {
+        if (todo.isComplete === true) {
             todoLocation.innerHTML += `
-            <tr class="completed">
-                <td class="wordWrap completed" scope="col">${todo.text}</td>  
-                <td class="completed" scope="col"><button disabled class="btn btn-sm btn-primary" onClick="markComplete(event,${todo.id}, false)">Completed</button></td>
-                <td class="completed" scope="col"><button class="btn btn-sm btn-danger deleteButton" onClick="deleteTodo(event,${todo.id})">Delete</button></td> 
+            <tr data-testid="toDoItem" class="completed">
+                <td class="completed" scope="col">${todo.text}</td>  
+                <td class="completed" scope="col"><button disabled class="btn btn-sm btn-primary" data-testid="completeButton" onClick="markComplete(event,${todo.id}, false)">Completed</button></td>
+                <td class="completed" scope="col"><button class="btn btn-sm btn-danger deleteButton" data-testid="deleteButton" onClick="deleteTodo(event,${todo.id})">Delete</button></td> 
             </tr>
     `;
 
@@ -109,36 +109,36 @@ function renderTodos(todos) {
     }
 }
 
-function markComplete(event,todoId, isComplete){
+function markComplete(event, todoId, isComplete) {
     event.preventDefault()
     console.log("Great Job! You completed todo: ", todoId);
     axios({
-     method: "PUT",
-     url: "/todos/complete/" + todoId,
-     data: {isComplete: isComplete}
+        method: "PUT",
+        url: "/todos/complete/" + todoId,
+        data: { isComplete: isComplete }
     })
-    .then((response) => {
-     getTodos()
-    })
-    .catch((error) => {
-     console.log('Error', error);
-     alert('Something went wrong');
-    });
-    }
+        .then((response) => {
+            getTodos()
+        })
+        .catch((error) => {
+            console.log('Error', error);
+            alert('Something went wrong');
+        });
+}
 
 
 function deleteTodo(event, todoId) {
-event.preventDefault()
+    event.preventDefault()
     axios({
-      method: "DELETE",
-      url: `/todos/${todoId}`
+        method: "DELETE",
+        url: `/todos/${todoId}`
     })
-      .then((response) => {
-        console.log('Deleting todo: ', todoId)
-        getTodos();
-      })
-      .catch((error) => {
-        console.log('Error', error);
-        alert('Something went wrong');
-      });
-  }
+        .then((response) => {
+            console.log('Deleting todo: ', todoId)
+            getTodos();
+        })
+        .catch((error) => {
+            console.log('Error', error);
+            alert('Something went wrong');
+        });
+}
