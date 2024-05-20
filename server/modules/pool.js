c41fea6ae6c19ae916c86c5d0aedbdb623c953bd
Sam Shacter <sshacter@gmail.com>
2024-05-20 15:27:07 -0500

    database change

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -6,11 +6,23 @@ if (process.env.NODE_ENV === 'test') {
   databaseName = 'prime_testing'
 }
 
-const pool = new pg.Pool({
+let pool = new pg.Pool({
     host: 'localhost',
     port: 5432,
     database: databaseName,
     allowExitOnIdle: true 
 })
+
+
+if (process.env.DATABASE_URL) {
+  pool = new pg.Pool({
+connectionString: process.env.DATABASE_URL,
+ssl: {
+  rejectUnauthorized: false
+}
+
+
+  })
+}
 
 module.exports = pool
